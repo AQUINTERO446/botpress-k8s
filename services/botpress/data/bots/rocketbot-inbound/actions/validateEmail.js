5392906a17c4d22732b290afd29dba383eb126e3
Andres Quintero Galvan <aquintero446@gmail.com>
2020-05-01 16:52:45 -0500

    all

---
+++ services/botpress/data/bots/rocketbot-inbound/actions/validateEmail.js
@@ -0,0 +1,12 @@
+  /**
+   * Small description of your action
+   * @title The title displayed in the flow editor
+   * @category Custom
+   * @author Your_Name
+   * @param {string} input -
+   */
+  const validate = async input => {
+    console.log(event)
+  }
+
+  return validate(args.name)
